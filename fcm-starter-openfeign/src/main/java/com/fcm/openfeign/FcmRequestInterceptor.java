13a14ed92c4ef663e21ac8702e0c71212d8c09ef
Tinhf0501 <nguyenhungtinh@192.168.2.15>
2024-11-02 23:56:36 +0700

    update FW

--- fcm-starter-openfeign/src/main/java/com/fcm/openfeign/FcmRequestInterceptor.java
+++ fcm-starter-openfeign/src/main/java/com/fcm/openfeign/FcmRequestInterceptor.java
@@ -3,13 +3,26 @@ package com.fcm.openfeign;
 import com.fcm.common.constant.AppConstants;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.MDC;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class FcmRequestInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
+        // Lấy HttpServletRequest hiện tại
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            // Ví dụ: Lấy header "Authorization" từ HttpServletRequest hiện tại
+            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+            if (authorizationHeader != null) {
+                requestTemplate.header(HttpHeaders.AUTHORIZATION, authorizationHeader);
+            }
+        }
         requestTemplate.header(AppConstants.TRACE_ID_KEY, MDC.get(AppConstants.TRACE_ID_KEY));
-        requestTemplate.header("Authorization", ""); // todo: set access token
     }
 }
